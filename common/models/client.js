59e52462bd91e2798f1f5e82870907aabf3c805d
Tiana Hayden <tiana.hayden@me.com>
2018-12-05 14:58:25 -0800

    added remote method for checking in and out

--- common/models/client.js
+++ common/models/client.js
@@ -1,5 +1,28 @@
 'use strict';
 
+const { checkInOut } = require('../../server/utils/checkInOut')
+
 module.exports = function(Client) {
-    
+    Client.checkInOut = function(username, cb) {
+        checkInOut(username)
+            .then(response => cb(null, response))
+            .catch(err => cb(err))
+    }
+
+    Client.remoteMethod('checkInOut' , {
+        description: 'Checks a client in or out',
+        accepts: {
+            arg: 'username',
+            type: 'string'
+        },
+        http: {
+            path: '/checkInOut',
+            verb: 'put'
+        },
+        returns: {
+            arg: 'data',
+            type: 'root',
+            root: true
+        }
+    })
 };
--- server/utils/checkInOut.js
+++ server/utils/checkInOut.js
@@ -5,14 +5,46 @@ function checkInOut(username) {
 		const { Client } = app.models;
 		Client.find({ where: { username: username} })
 			.then((client) => {
-				if (client.currentlyCheckedIn === false) {
+				if (client[0].currentlyCheckedIn == false) {
+					// why doesn't the spread operator work here?
 					const updateClient = {
-						...client,
+						"firstName": client[0].firstName,
+						"lastName": client[0].lastName,
+						"location": client[0].location,
+						"tableclothClientId": client[0].tableclothClientId,
+						"gender": client[0].gender,
+						"birthday": client[0].birthday,
+						"gradeAtSignup": client[0].gradeAtSignup,
+						"signUpDate": client[0].signUpDate,
 						"currentlyCheckedIn": true,
-					}
+						"username": client[0].username,
+						"id": client[0].id
+					  }
 					Client.replaceOrCreate(updateClient)
 						.then(() => resolve(updateClient))
 						.catch(err => reject(err));
+				}
+				if (client[0].currentlyCheckedIn == true) {
+					// why doesn't the spread operator work here?
+					const updateClient = {
+						"firstName": client[0].firstName,
+						"lastName": client[0].lastName,
+						"location": client[0].location,
+						"tableclothClientId": client[0].tableclothClientId,
+						"gender": client[0].gender,
+						"birthday": client[0].birthday,
+						"gradeAtSignup": client[0].gradeAtSignup,
+						"signUpDate": client[0].signUpDate,
+						"currentlyCheckedIn": false,
+						"username": client[0].username,
+						"id": client[0].id
+					  }
+					Client.replaceOrCreate(updateClient)
+						.then(() => resolve(updateClient))
+						.catch(err => reject(err));
+				}
+				else {
+					console.log('wrong')
 				}
 			})
 			.catch(err => reject({ error: 'Could not find username', err }))
@@ -21,4 +53,4 @@ function checkInOut(username) {
 
 
 
-module.exports = { changeTalkContent };
+module.exports = { checkInOut };
